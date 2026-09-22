d67163be365295e723b032fd3a35647b725e8fc9
DePip892 <ruizmichael16@gmail.com>
2026-09-22 11:06:34 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Offline cache for the Budget app. Bump CACHE when you replace index.html. */
-var CACHE = "budget-v10";
+var CACHE = "budget-v11";
 var ASSETS = ["./", "./index.html"];
 
 self.addEventListener("install", function(e){
